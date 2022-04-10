d41cd5741d5ec58ef5db78737f39f8d27167a22d
Edson Tomas <54870064+EdsonTomas@users.noreply.github.com>
2022-04-10 23:49:31 +0100

    projecto concluido

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const img = document.getElementById( 'img' );
-const buttons = document.getElementById( 'buttons' );
-let colorIndex = 0;
+const img       =   document.getElementById( 'img' );
+const buttons   =   document.getElementById( 'buttons' );
 
-const semaforo = (event)=>{
+let indexColor  = 0;
+let intervalId = null;
+
+buttons.addEventListener('click',(event)=>{
+    clearInterval(intervalId);
     turnOn[event.target.id]();
-}
+});
 
-const changeColor = ()=>{
-    const colors    = ['red','yellow','green'];
-    const color     = colors[colorIndex];
-    turnOn[color]();
-    if(colorIndex < 2){
-        colorIndex++;
+const trocarCor = ()=>{
+    
+    const cores =   ['red','yellow','green'];
+    const cor   =   cores[indexColor];    
+    turnOn[cor]();
+   
+    if(indexColor < 2){
+        indexColor ++
     }else{
-        colorIndex = 0;
+        indexColor = 0;
     }
+    console.log(indexColor);
 }
-buttons.addEventListener('click',semaforo);
-
 
 const turnOn = {
-    'red'       :   ()=> img.src = './img/vermelho.png',
-    'yellow'    :   ()=> img.src = './img/amarelo.png',
-    'green'     :   ()=> img.src = './img/verde.png',
-    'automatic' :   ()=> setInterval(changeColor, 1000)
+    'red'    :      ()=> img.src = './img/vermelho.png',
+    'yellow' :      ()=> img.src = './img/amarelo.png',
+    'green'  :      ()=> img.src = './img/verde.png',
+    'automatic':    ()=> intervalId = setInterval(trocarCor,1000)
 }
